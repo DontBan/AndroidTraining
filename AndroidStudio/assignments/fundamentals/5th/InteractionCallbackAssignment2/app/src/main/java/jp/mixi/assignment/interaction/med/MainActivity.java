27a3b222f63f759bd5c858cf5205439c77b83c52
Tomohiro <noraware29@gmail.com>
2016-05-17 22:44:35 +0900

    [update]5-2-4 finish.

--- AndroidStudio/assignments/fundamentals/5th/InteractionCallbackAssignment2/app/src/main/java/jp/mixi/assignment/interaction/med/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/5th/InteractionCallbackAssignment2/app/src/main/java/jp/mixi/assignment/interaction/med/MainActivity.java
@@ -3,9 +3,45 @@ package jp.mixi.assignment.interaction.med;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            TextView counter = (TextView) findViewById(R.id.countEditText);
+            counter.setText(String.valueOf(editable.length()));
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.addTextChangedListener(mTextWatcher);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.removeTextChangedListener(mTextWatcher);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
