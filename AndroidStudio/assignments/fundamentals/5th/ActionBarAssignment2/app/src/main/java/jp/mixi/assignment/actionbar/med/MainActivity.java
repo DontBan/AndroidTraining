1ffd128d54886a3b36ea5d2bd459cb65207a8372
Tomohiro <noraware29@gmail.com>
2016-05-17 14:49:51 +0900

    [update]ActionBarAssignment2 finisth.

--- AndroidStudio/assignments/fundamentals/5th/ActionBarAssignment2/app/src/main/java/jp/mixi/assignment/actionbar/med/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/5th/ActionBarAssignment2/app/src/main/java/jp/mixi/assignment/actionbar/med/MainActivity.java
@@ -1,23 +1,65 @@
 
 package jp.mixi.assignment.actionbar.med;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity implements TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Sherlock);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(getSupportActionBar().newTab().setText("Tab1").setTabListener(this));
+        actionBar.addTab(getSupportActionBar().newTab().setText("Tab2").setTabListener(this));
+        actionBar.addTab(getSupportActionBar().newTab().setText("Tab3").setTabListener(this));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+                Intent intent = new Intent(this, NewActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
 }
