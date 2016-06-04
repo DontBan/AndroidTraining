6e6cae6313e2169b2ea91eae3e71b0c2eea2fdf7
Tomohiro <noraware29@gmail.com>
2016-06-04 19:58:34 +0900

    [update]AT-7-Collection-1 finish

--- AndroidStudio/practice/fundamentals/7th/SerializablePractice1/app/src/main/java/jp/mixi/practice/serializable/MainActivity.java
+++ AndroidStudio/practice/fundamentals/7th/SerializablePractice1/app/src/main/java/jp/mixi/practice/serializable/MainActivity.java
@@ -3,6 +3,16 @@ package jp.mixi.practice.serializable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +26,49 @@ public class MainActivity extends Activity {
         NetworkClient client = new NetworkClient();
         String user = client.getUser(123);
 
+        // JSONの文字列userを解析しUserクラスに入れる
+        User you = new User();
+        try {
+            JSONObject jsonObject = new JSONObject(user);
+            you.setId(jsonObject.getInt("id"));
+            you.setName(jsonObject.getString("name"));
+            // 年齢の記載が無い場合がある
+            if (jsonObject.has("age")) {
+                you.setAge(jsonObject.getInt("age"));
+            } else {
+                you.setAge(0);
+            }
+            // キーワードの記載がない場合がある
+            if (jsonObject.has("keyword")) {
+                you.setKeyword(jsonObject.getString("keyword"));
+            } else {
+                you.setKeyword("");
+            }
+            // 入れ子のJSONObject
+            JSONObject status = jsonObject.getJSONObject("status");
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.US);
+            String postedTime = status.getString("postedTime");
+            Date d = sdf.parse(postedTime);
+            User.Status s = new User.Status();
+            s.setPostedDate(d);
+            s.setText(status.getString("text"));
+            you.setStatus(s);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // activity_mainに反映
+        TextView userId = (TextView) findViewById(R.id.userId);
+        userId.setText(String.valueOf(you.getId()));
+        TextView userName = (TextView) findViewById(R.id.userName);
+        userName.setText(you.getName());
+        TextView userAge = (TextView) findViewById(R.id.userAge);
+        userAge.setText(String.valueOf(you.getAge()));
+        TextView userKeyword = (TextView) findViewById(R.id.userKeyword);
+        userKeyword.setText(you.getKeyword());
+        TextView userStatusText = (TextView) findViewById(R.id.userStatusText);
+        userStatusText.setText(you.getStatus().getText());
     }
 
     @Override
--- AndroidStudio/practice/fundamentals/7th/SerializablePractice1/app/src/main/java/jp/mixi/practice/serializable/User.java
+++ AndroidStudio/practice/fundamentals/7th/SerializablePractice1/app/src/main/java/jp/mixi/practice/serializable/User.java
@@ -1,14 +1,53 @@
 package jp.mixi.practice.serializable;
 
-import java.util.Date;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class User {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class User implements Parcelable {
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
     private String name;
     private int id;
     private int age;
     private String keyword;
     private Status status;
-    
+
+    public User() {}
+    private User(Parcel in) {
+        name = in.readString();
+        id = in.readInt();
+        age = in.readInt();
+        keyword = in.readString();
+        status = in.readParcelable(Status.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeInt(id);
+        parcel.writeInt(age);
+        parcel.writeString(keyword);
+        parcel.writeParcelable(status, 0);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
     public String getName() {
         return name;
     }
@@ -49,7 +88,37 @@ public class User {
         this.keyword = keyword;
     }
 
-    public static class Status{
+    public static class Status implements Parcelable {
+        public static final Parcelable.Creator<Status> CREATOR = new Parcelable.Creator<Status>() {
+            public Status createFromParcel(Parcel in) {
+                return new Status(in);
+            }
+            public Status[] newArray(int size) {
+                return new Status[size];
+            }
+        };
+        public Status() {}
+        private Status(Parcel in) {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd hh:mm:ss zzz yyyy", Locale.US);
+            try {
+                mPostedDate = sdf.parse(in.readString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            mText = in.readString();
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            parcel.writeString(mPostedDate.toString());
+            parcel.writeString(mText);
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
         private Date mPostedDate;
         private String mText;
         public Date getPostedDate() {
