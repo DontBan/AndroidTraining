0568fe9e5a7d81c007b79593ab671caddae88651
Tomohiro <noraware29@gmail.com>
2016-06-21 21:43:29 +0900

    [update]add countup

--- AndroidStudio/assignments/fundamentals/7th/SharedPreferencesAssignment/app/src/main/java/jp/mixi/assignment/sharedpreferences/sharedpreferencesassignment/MainActivity.java
+++ AndroidStudio/assignments/fundamentals/7th/SharedPreferencesAssignment/app/src/main/java/jp/mixi/assignment/sharedpreferences/sharedpreferencesassignment/MainActivity.java
@@ -3,13 +3,30 @@ package jp.mixi.assignment.sharedpreferences.sharedpreferencesassignment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    // カウンター
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: カウントアップ機能を作る
+        findViewById(R.id.countup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                TextView countTextView = (TextView) findViewById(R.id.count);
+                countTextView.setText(String.valueOf(counter));
+            }
+        });
+        //TODO: カウントを保存する
+        //TODO: onSharedPreferenceChangedから変更する
     }
 
     @Override
